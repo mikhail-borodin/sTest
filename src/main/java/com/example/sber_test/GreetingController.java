84b0ddc2ba8e56ab2a3a7944797d48090a58e725
mikhail.borodin <mikhail.borodin@deuscraft.com>
2019-01-18 09:33:18 +0700

    first

--- src/main/java/com/example/sber_test/GreetingController.java
+++ src/main/java/com/example/sber_test/GreetingController.java
@@ -12,29 +12,33 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+
     @Autowired
     private TransactionRepository repository;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }
+//    @GetMapping("/greeting")
+//    public String greeting(
+//            @RequestParam(name="name", required=false, defaultValue="World") String name,
+//            Map<String, Object> model
+//    ) {
+//        model.put("name", name);
+//        return "greeting";
+//    }
 
     @GetMapping
     public String main(Map<String,Object> model){
         Iterable<Transaction> transactions = repository.findAll();
         model.put("transactions", transactions);
+
+        System.out.println("GetMapping ");
+
         return "main";
     }
 
     @PostMapping
     public String add(@RequestParam String sender, @RequestParam String recipient, @RequestParam String amount, Map<String,Object> model) {
-        Transaction transaction = new Transaction(sender, recipient, amount);
 
+        Transaction transaction = new Transaction(sender, recipient, amount);
         repository.save(transaction);
 
         Iterable<Transaction> transactions = repository.findAll();
--- src/main/java/com/example/sber_test/repository/TransactionRepository.java
+++ src/main/java/com/example/sber_test/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package com.example.sber_test.repository;
 import com.example.sber_test.domain.Transaction;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 }
